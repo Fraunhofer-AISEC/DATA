9f2637ec180aa60108471cbe7b69c0eedb7b3ba0
Alexander Wagner <alexander.wagner@ulal.de>
2022-03-18 14:59:51 +0100

    pintool: Make clang-format happy
    Run with: clang-format -style="{IndentWidth: 4}" -i *.{c*,h*,H}

--- pintool/sha1.hpp
+++ pintool/sha1.hpp
@@ -20,26 +20,22 @@
 #ifndef SHA1_HPP
 #define SHA1_HPP
 
-
 #include <cstdint>
 #include <iostream>
 #include <string>
 
-
-class SHA1
-{
-public:
+class SHA1 {
+  public:
     SHA1();
     void update(const std::string &s);
     void update(std::istream &is);
     std::string final();
     static std::string from_file(const std::string &filename);
 
-private:
+  private:
     uint32_t digest[5];
     std::string buffer;
     uint64_t transforms;
 };
-
 
 #endif /* SHA1_HPP */
