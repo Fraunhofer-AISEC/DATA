76c029d7a83ed52b863f0a611eaf173e137f8207
Alexander Wagner <alexander.wagner@ulal.de>
2023-09-08 11:26:41 +0200

    cryptolib/botan: Add ECDH scripts

---
+++ cryptolib/botan/algo_ecdh/data_run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+PINFLAGS="--phase1 --phase2 --export --parallel"
+export RESULTDIR=results
+
+
+pushd ${BASH_SOURCE%/*}
+
+if [[ $1 == "clean" || $2 == "clean" ]]; then
+    rm -rf results
+fi
+
+./framework.sh ${PINFLAGS} secp256r1
+
+if [[ $1 == "test" || $2 == "test" ]]; then
+    popd
+    exit 0
+fi
+
+./framework.sh ${PINFLAGS}
+
+popd
